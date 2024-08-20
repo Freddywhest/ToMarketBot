2576308c90bc5c9727406a51ab90d339aac1bfc3
FreddyWhest <alfrednti466@gmail.com>
2024-08-20 16:38:07 +0000

    Added claiming combo and claiming stars

--- bot/config/config.js
+++ bot/config/config.js
@@ -20,6 +20,14 @@ const settings = {
     ? process.env.AUTO_FARM.toLowerCase() === "true"
     : true,
 
+  AUTO_CLAIM_COMBO: process.env.AUTO_CLAIM_COMBO
+    ? process.env.AUTO_CLAIM_COMBO.toLowerCase() === "true"
+    : true,
+
+  AUTO_CLAIM_STARTS: process.env.AUTO_CLAIM_STARTS
+    ? process.env.AUTO_CLAIM_STARTS.toLowerCase() === "true"
+    : true,
+
   SLEEP_BETWEEN_TAP: process.env.SLEEP_BETWEEN_TAP
     ? process.env.SLEEP_BETWEEN_TAP.split(",").map((str) =>
         parseInt(str.trim())
